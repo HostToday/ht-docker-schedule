7739bca84cc9ed364b00383bdb2d110a591ad489
Phil Kunz <github@philkunz.com>
2016-04-25 04:48:03 +0200

    add git module

--- app-node/ts/index.ts
+++ app-node/ts/index.ts
@@ -4,6 +4,7 @@ import plugins = require("./schedule.plugins");
 import ScheduleCoresock = require("./schedule.coresock");
 import ScheduleEnvironment = require("./schedule.environment");
 import ScheduleEvents = require("./schedule.events");
+import ScheduleGit = require("./schedule.git");
 import ScheduleUnitfile = require("./schedule.unitfile");
 
 plugins.beautylog.log("modules loaded! now starting ticker!");
---
+++ app-node/ts/schedule.git.ts
@@ -0,0 +1,6 @@
+/// <reference path="./typings/main.d.ts" />
+import plugins = require("./schedule.plugins");
+
+export let pull = function(){
+    
+}
